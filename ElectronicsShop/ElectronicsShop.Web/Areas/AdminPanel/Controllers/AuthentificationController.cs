2e658721adb800e439179b2b9c31fbcc4f92a945
Ursul Vladislav <vladislav.ursul@isa.utm.md>
2023-03-10 17:30:11 +0200

    admin dashboard template integration

---
+++ ElectronicsShop/ElectronicsShop.Web/Areas/AdminPanel/Controllers/AuthentificationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicsShop.Web.Areas.AdminPanel.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        // GET: AdminPanel/Authentification
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // GET: AdminPanel/Authentification
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
