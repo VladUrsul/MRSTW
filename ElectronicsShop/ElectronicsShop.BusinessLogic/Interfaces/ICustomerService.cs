529e3e54d3410546db2335070b0193b00a930f42
Ursul Vladislav <vladislav.ursul@isa.utm.md>
2023-03-23 16:00:01 +0200

    business logic init

---
+++ ElectronicsShop/ElectronicsShop.BusinessLogic/Interfaces/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using ElectronicsShop.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicsShop.BusinessLogic.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task AddCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+    }
+
+}
---
+++ ElectronicsShop/ElectronicsShop.BusinessLogic/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using ElectronicsShop.BusinessLogic.Interfaces;
+using ElectronicsShop.BusinessLogic.Models;
+using ElectronicsShop.BusinessLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicsShop.BusinessLogic.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ShopDbContext _context;
+
+        public ProductService(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
