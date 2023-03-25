3dd1fa50a1547842f0b3359429f902f2c918d901
Ursul Vladislav <vladislav.ursul@isa.utm.md>
2023-03-25 10:19:32 +0200

    store area init

---
+++ ElectronicsShop/ElectronicsShop.Web/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicsShop.Web.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
