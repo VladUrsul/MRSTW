9f036d852bf2f108c4ab4cffad399245aa49f35f
Ursul Vladislav <vladislav.ursul@isa.utm.md>
2023-04-27 17:37:41 +0300

    Users Page

--- ElectronicsShop/ElectronicsShop.Web/Areas/AdminPanel/Controllers/UsersController.cs
+++ ElectronicsShop/ElectronicsShop.Web/Areas/AdminPanel/Controllers/UsersController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ElectronicsShop.Data.Contexts;
 
 namespace ElectronicsShop.Web.Areas.AdminPanel.Controllers
 {
     public class UsersController : Controller
     {
         // GET: AdminPanel/User
-        public ActionResult Index()
+        private readonly ShopDbContext _context;
+
+        public UsersController(ShopDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public ActionResult Users()
+        {
+            var users = _context.Users.ToList();
+            return View("~/Areas/AdminPanel/Views/Users/Users.cshtml", users);
         }
     }
 }
