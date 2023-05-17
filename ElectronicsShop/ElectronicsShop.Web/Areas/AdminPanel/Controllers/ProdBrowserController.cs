ac72b2c81d30856995a2c9f607a4006bf7d4a7fc
Alex <nicolay-alex.aoun@isa.utm.md>
2023-05-17 18:08:17 +0300

    Products browser and viewer

---
+++ ElectronicsShop/ElectronicsShop.Web/Areas/AdminPanel/Controllers/ProdBrowserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicsShop.Web.Areas.AdminPanel.Controllers
+{
+    public class ProdBrowserController : Controller
+    {
+        public ActionResult ProdBrowser()
+        {
+            return View("~/Areas/AdminPanel/Views/ProdBrowser/ProdBrowser.cshtml");
+        }
+    }
+}
